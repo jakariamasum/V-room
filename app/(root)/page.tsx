a4d3fbb3e56adb3b2c96648070e8fae0389673cb
jakariamasum <jmphy698@gmail.com>
2025-01-30 23:53:40 +0600

    banner

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,10 @@
+import Banner from "@/components/banner";
+
 export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       init section
+      <Banner />
     </section>
   );
 }
